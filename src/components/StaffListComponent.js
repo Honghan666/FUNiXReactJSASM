404951b4a45f2924db5624f4c0fd49eeb6ebd1f5
Hong Han <honghanhg1607@gmail.com>
2022-06-01 22:25:30 +0700

    code updated 3

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,40 +1,33 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardTitle, CardBody, CardText } from 'reactstrap';
 import dateFormat from 'dateformat';
 
 class StaffList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            onSelectedStaff: null
+            check: null
         };
     }
 
     onStaffClick(staff) {
         this.setState({
-            onSelectedStaff: staff
+            check: staff
         });
     }
 
-    renderStaff(staff) {
-        if( staff != null) {
+    staffDetail(staff) {
+        if(staff != null) {
             return (
-                <div className='col-12'>
-                    <Card>
-                        <CardImg width="100%" src={staff.image} alt={staff.name} />
-                        <CardBody>
-                            <CardTitle>Họ và Tên: {staff.name}</CardTitle>
-                            <CardText>
-                                Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
-                            </CardText>
-                            <CardText>
-                                Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
-                            </CardText>
-                            <CardText>Phòng ban: {staff.department.name}</CardText>
-                            <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
-                            <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
-                        </CardBody>
-                    </Card>
+                <div className='col-5 mx-auto card'>
+                    <img width="35%" src={staff.image} alt={staff.name} className='rounded mx-auto d-block' />
+                        <div className='card-body'>
+                            <p>Họ và Tên: {staff.name}</p>
+                            <p>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</p>
+                            <p>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</p>
+                            <p>Phòng ban: {staff.department.name}</p>
+                            <p>Số ngày nghỉ còn lại: {staff.annualLeave}</p>
+                            <p>Số ngày đã làm thêm: {staff.overTime}</p>
+                        </div>
                 </div>
             );
         }
@@ -43,18 +36,16 @@ class StaffList extends Component {
         }
     }
 
-
-
     render() {
         console.log(this.props.staffs)
         const menustaff = this.props.staffs.map((staff) => {
             return (
                     <div className='col-12 col-md-6 col-lg-4 mt-3'>
-                        <Card key={staff.id} onClick={() => this.onStaffClick(staff)}>
-                            <CardBody>
-                                <CardTitle>{staff.name}</CardTitle>
-                            </CardBody>
-                        </Card>
+                        <div className='card' key={staff.id} onClick={() => this.onStaffClick(staff)}>
+                            <div className='card-body'>
+                                <p>{staff.name}</p>
+                            </div>
+                        </div>
                     </div>
             );
         });
@@ -62,7 +53,7 @@ class StaffList extends Component {
             <div className='container'>
                 <div className='row'>{menustaff}</div>
                 <div className='row mt-3'>
-                    {this.renderStaff(this.state.onSelectedStaff)}
+                    {this.staffDetail(this.state.check)}
                 </div>
             </div>
         );
