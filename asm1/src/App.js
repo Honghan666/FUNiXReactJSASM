5a3e8f868968be2b5586a4d3c2a6125d933cbb57
Hong Han <honghanhg1607@gmail.com>
2022-06-01 21:15:20 +0700

    install date format

--- asm1/src/App.js
+++ asm1/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import StaffList from './components/StaffListComponent';
+import {STAFFS} from './components/staffs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      staffs: STAFFS
+    }
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color='primary'>
+          <div className='container'>
+            <NavbarBrand href='/'>Ung dung quan ly nhan su v1.0</NavbarBrand>
+          </div>
+        </Navbar>
+        <StaffList staffs={this.state.staffs} />
+      </div>
+    )
+  }
 }
 
 export default App;
+
